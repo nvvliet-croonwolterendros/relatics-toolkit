5c5aae4689e51a63541e24dc98d7420ab90e3e16
Nick van Vliet <nick.vanvliet@croonwolterendros.nl>
2026-09-21 13:37:04 +0200

    refactor(utils): Moved extra special character mapping from transformer to utils

--- src/relatics_toolkit/utils/utils.py
+++ src/relatics_toolkit/utils/utils.py
@@ -16,7 +16,18 @@ def normalize_value(val: str | None, max_length: int = 63) -> str | None:
     val = str(val)
 
     # Replace special characters
-    val = val.replace("&", "_en_").replace("€", "_euro_").replace("+", "_plus_")
+    special_mapping = str.maketrans({
+        "&": "_en_",
+        "€": "_euro_",
+        "+": "_plus_",
+        "%": "_procent_",
+        "#": "_nr_",
+        "§": "_paragraaf_",
+        "$": "_dollar_",
+        "=": "_is_",
+        "°": "_graden_",
+    })
+    val = val.translate(special_mapping)
 
     # Unicode -> ASCII
     val = unicodedata.normalize("NFKD", val)
